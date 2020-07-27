dfe021b0f953aa1815ac61a82ab517954b2dfff1
serain <alex@kaskaso.li>
2020-07-27 22:24:54 +0100

    simplify test

--- tests/test_programs.py
+++ tests/test_programs.py
@@ -6,16 +6,8 @@ from bbsearch import BugBountySearch, models
 bb = BugBountySearch(os.environ.get("BBSEARCH_API_KEY"))
 
 
-def test_get_program():
-    program = bb.get_program("gate-io-exchange")
-    assert isinstance(program, models.Program)
-    assert validators.url(program.program_url)
-    assert program.platform == "hackenproof"
-    assert "web" in program.types
-    assert len(program.in_scope) > 0
-
-
 def test_get_programs():
-    programs = bb.get_programs()
+    programs = bb.programs()
     program = next(programs)
     assert isinstance(program, models.Program)
+    assert validators.url(program.program_url)
