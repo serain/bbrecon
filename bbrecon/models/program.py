6051cbf3125df9f146b8d9b2cc1df3e82a032e51
serain <alex@kaskaso.li>
2020-08-03 00:35:09 +0100

    clean up program model

--- bbrecon/models/program.py
+++ bbrecon/models/program.py
@@ -9,8 +9,6 @@ from .program_scope import ProgramScope
 
 @dataclass
 class Program:
-    """  """
-
     program_url: str
     program_name: str
     platform: str
@@ -22,15 +20,12 @@ class Program:
     created_at: datetime
     updated_at: datetime
     slug: str
+    live: bool
+    minimum_bounty: int
+    maximum_bounty: int
+    average_bounty: int
 
     def to_dict(self) -> Dict[str, Any]:
-        program_url = self.program_url
-        program_name = self.program_name
-        platform = self.platform
-        rewards = self.rewards
-
-        types = self.types
-
         in_scope = []
         for in_scope_item_data in self.in_scope:
             in_scope_item = in_scope_item_data.to_dict()
@@ -43,72 +38,52 @@ class Program:
 
             out_scope.append(out_scope_item)
 
-        bounty_created_at = self.bounty_created_at.isoformat()
-
-        created_at = self.created_at.isoformat()
-
-        updated_at = self.updated_at.isoformat()
-
-        slug = self.slug
-
         return {
-            "programUrl": program_url,
-            "programName": program_name,
-            "platform": platform,
-            "rewards": rewards,
-            "types": types,
+            "programUrl": self.program_url,
+            "programName": self.program_name,
+            "platform": self.platform,
+            "rewards": self.rewards,
+            "types": self.types,
             "in_scope": in_scope,
             "out_scope": out_scope,
-            "bountyCreatedAt": bounty_created_at,
-            "createdAt": created_at,
-            "updatedAt": updated_at,
-            "slug": slug,
+            "bountyCreatedAt": self.bounty_created_at.isoformat(),
+            "createdAt": self.created_at.isoformat(),
+            "updatedAt": self.updated_at.isoformat(),
+            "slug": self.slug,
+            "live": self.live,
+            "minimumBounty": self.minimum_bounty,
+            "maximumBounty": self.maximum_bounty,
+            "averageBounty": self.average_bounty,
         }
 
     @staticmethod
     def from_dict(d: Dict[str, Any]) -> Program:
-        program_url = d["programUrl"]
-
-        program_name = d["programName"]
-
-        platform = d["platform"]
-
-        rewards = d["rewards"]
-
-        types = d["types"]
-
         in_scope = []
-        for in_scope_item_data in d["in_scope"]:
+        for in_scope_item_data in d["inScope"]:
             in_scope_item = ProgramScope.from_dict(in_scope_item_data)
-
             in_scope.append(in_scope_item)
 
         out_scope = []
-        for out_scope_item_data in d["out_scope"]:
+        for out_scope_item_data in d["outScope"]:
             out_scope_item = ProgramScope.from_dict(out_scope_item_data)
-
             out_scope.append(out_scope_item)
 
-        bounty_created_at = datetime.strptime(
-            d["bountyCreatedAt"], "%Y-%m-%dT%H:%M:%SZ"
-        )
-
-        created_at = datetime.strptime(d["createdAt"], "%Y-%m-%dT%H:%M:%SZ")
-
-        updated_at = datetime.strptime(d["updatedAt"], "%Y-%m-%dT%H:%M:%SZ")
-
-        slug = d["slug"]
-
         return Program(
-            program_url=program_url,
-            program_name=program_name,
-            platform=platform,
-            rewards=rewards,
-            types=types,
+            program_url=d["programUrl"],
+            program_name=d["programName"],
+            platform=d["platform"],
+            rewards=d["rewards"],
+            types=d["types"],
             in_scope=in_scope,
             out_scope=out_scope,
-            bounty_created_at=bounty_created_at,
-            created_at=created_at,
-            updated_at=updated_at,
-            slug=slug,
+            bounty_created_at=datetime.strptime(
+                d["bountyCreatedAt"], "%Y-%m-%dT%H:%M:%SZ"
+            ),
+            created_at=datetime.strptime(d["createdAt"], "%Y-%m-%dT%H:%M:%SZ"),
+            updated_at=datetime.strptime(d["updatedAt"], "%Y-%m-%dT%H:%M:%SZ"),
+            slug=d["slug"],
+            live=d["live"],
+            minimum_bounty=d["minimumBounty"],
+            maximum_bounty=d["maximumBounty"],
+            average_bounty=d["averageBounty"],
         )
